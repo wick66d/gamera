46927f0d229d75a7887e235571caf4a60b958388
wick66d <gustasgre@gmail.com>
2025-12-02 23:35:45 +0200

    Added mongodb.entities, added mongo to docker, initialized it, added the listing model to it.

---
+++ src/SearchService/Models/Listing.cs
@@ -0,0 +1,24 @@
+using System;
+using MongoDB.Entities;
+
+namespace SearchService.Models;
+
+public class Listing : Entity
+{
+    public Guid SellerId { get; set; }
+    public Guid GameId { get; set; }
+    public string GameName { get; set; }
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public decimal PriceAmount { get; set; }
+    public string Currency { get; set; }
+    public bool IsStackable { get; set; }
+    public int QuantityAvailable { get; set; }
+    public string DeliveryType { get; set; }
+    public int MinDeliveryMinutes { get; set; }
+    public int MaxDeliveryMinutes { get; set; }
+    public string Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -5,5 +9,15 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 app.MapControllers();
+
+await DB.InitAsync("SearchDb", MongoClientSettings
+    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+await DB.Index<Listing>()
+    .Key(x => x.Title, KeyType.Text)
+    .Key(x => x.CategoryName, KeyType.Text)
+    .Key(x => x.GameName, KeyType.Text)
+    .CreateAsync();
+
 
 app.Run();
